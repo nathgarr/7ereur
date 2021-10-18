3add94fd6769155af444bf2c6d04c4dcebef5b0b
nathgarr <nathgarr@outlook.fr>
2021-10-18 10:15:10 +0200

    feat:opacity js

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 
+function respawn (){
 
+    const dessuLamp = document.getElementById('dessulamp')
+    const barreLamp = document.getElementById('barrelamp')
+    const cannaper = document.getElementById('cannap')
+    const enfant = document.getElementById('enfant')
+    const enseigne = document.getElementById('enseigne')
+    const menus = document.getElementById('menus')
+    const horloge = document.getElementById('num2')
+    
+    dessuLamp.addEventListener('click', () =>{
+        dessuLamp.style.opacity = 1
+    })
+    barreLamp.addEventListener('click', () =>{
+        barreLamp.style.opacity = 1
+    })
+    cannaper.addEventListener('click', () =>{
+        cannaper.style.opacity = 1
+    })
+    enfant.addEventListener('click', () =>{
+        enfant.style.opacity = 1
+    })
+    enseigne.addEventListener('click', () =>{
+        enseigne.style.opacity = 1
+    })
+    menus.addEventListener('click', () =>{
+        menus.style.opacity = 1
+    })
+    horloge.addEventListener('click', () =>{
+        horloge.style.opacity = 1
+    })
+}
+respawn();
